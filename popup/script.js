c0ff4eda343b7725f3f953b60fd2bcf7cf4b1e41
mitjaprasnikar <mitja.prasnikar@gmail.com>
2020-02-08 19:11:48 +0100

    Simple popup

---
+++ popup/script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("btn");
+const popup = document.getElementById("popup");
+const close = document.getElementById("close");
+
+
+openPopup = () => {
+    popup.style.display = "block";
+}
+
+closePopup = () => {
+    popup.style.display = "none";
+}
